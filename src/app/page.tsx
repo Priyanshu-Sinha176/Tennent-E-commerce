7e93aa6ea67893fe1d547424ab8f084204ffc4cd
Priyanshu Kumar <sinhapriyanshu176@gmail.com>
2025-05-02 23:59:13 +0530

    02: Customisation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input';
+import { Progress } from '@/components/ui/progress';
+import { Textarea } from '@/components/ui/textarea';
+import { Checkbox } from '@/components/ui/checkbox';
 
 const page = () => {
   return (
-    <div>
+    <div className='p-4'>
 
-      <Button variant="a"> click </Button>
+      <p className='text-rose-500 '> Hello World </p>
 
-      <p className='text-rose-500'> Hello World </p>
-      
+      <div className="flex flex-col gap-y-6">
+
+        <div> <Button variant="elevated"> This is a Button </Button> </div>
+
+        <div> <Input placeholder='I am an Input' />  </div>
+
+        <div> <Progress value={50}/> </div>
+
+        <div> <Textarea placeholder='I am a Text Area' /> </div>
+
+        <div> <Checkbox /> </div>
+
+      </div>
+
     </div>
+
   );
 }
 
